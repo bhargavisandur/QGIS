d707413ffe6ced666fa9c006257d89b18eb9a3ec
AtharvaThorve <aaathorve@gmail.com>
2020-03-15 16:41:38 +0530

    DB configuration complete

--- bin/configuredb.sh
+++ bin/configuredb.sh
@@ -2,14 +2,22 @@
 
 database="Project DB"
 
+if psql -U postgres -t -c '\du' | cut -d \| -f 1 | grep -qw me;
+then
+	echo "Role 'me' already exists with password 'password'"
+else
+	psql -U postgres -c "CREATE USER me WITH SUPERUSER PASSWORD 'password';"
+	echo "Created role 'me' with password 'password'"
+fi
+
 echo "Configuring $database"
 
 if psql -U postgres -lqt | cut -d \| -f 1 | grep -qw appdb;
 then
-	dropdb -U postgres appdb
+	dropdb -U me appdb
 fi
-createdb -U postgres appdb
+createdb -U me appdb
 
-psql -U postgres appdb < ./bin/sql/appdb.sql
+psql -U me appdb < ./bin/sql/appdb.sql
 
 echo "$database configured"
