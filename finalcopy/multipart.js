523ef24adb63331034155b70842f0969a88dda5b
Bhargavi Sandur <bssandur_b18@it.vjti.ac.in>
2020-04-19 13:06:37 +0530

    add the nodemailer file and also add the adminPage which displays the victims

---
+++ finalcopy/multipart.js
@@ -0,0 +1,34 @@
+// To add images to the server storage
+const multer = require("multer");
+const path = require("path");
+const crypto = require("crypto");
+
+const storage = multer.diskStorage({
+    destination: "./public/images",
+    filename: (req, file, cb) => {
+        return crypto.pseudoRandomBytes(16, function (err, raw) {
+            if (err) {
+                return cb(err);
+            }
+            return cb(
+                null,
+                "" + raw.toString("hex") + path.extname(file.originalname)
+            );
+        });
+    },
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+const multerObject = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+}).single("image");
+
+module.exports = multerObject;
---
+++ finalcopy/nodeMailer/sendEmail.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+const transport = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'noabuseapp@gmail.com',
+        pass: 'noabuse123',
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
+module.exports = function sendEmail(to, subject, message) {
+    const mailOptions = {
+        from: 'noabuseapp@gmail.com',
+        to,
+        subject,
+        html: message,//'<h1>Thank you for using the NoAbuse App and helping another child!</h1><h3>To know more about the child that you reported and to join our amazing community, click the link below</h3><a href="https://localhost:3000/userRegister">Register Now!</a>'
+    };
+
+    transport.sendMail(mailOptions, (error) => {
+        if (error) {
+            console.log(error);
+        }
+    });
+};
